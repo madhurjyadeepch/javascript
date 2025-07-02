a309add2e56651ba171988ac690a133d7f79f021
Madhurjya Deep Chakravarty <madhurjyadeepch@gmail.com>
2025-07-02 22:50:41 +0530

    Learn about data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alert (3+3) we are using nodejs, not browser
+
+console.log("Madhurjya")
+
+let name = "Madhurjya"
+let age = 20
+let isLoggedIn = false
+
+
+// number = 2^53
+// bigint = for large numbers
+// string = ""
+// boolean = true or false
+// null = standalone value (type = object)
+// undefined (type = undefined)
+// symbol = uniqueness
+
+console.log(typeof "Madhurjya")
+console.log(typeof null)
+console.log(typeof undefined)
+
+
