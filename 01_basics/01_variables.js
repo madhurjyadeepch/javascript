00be5235cc212920873e7e67e74b01a1a4472e7a
Madhurjya Deep Chakravarty <madhurjyadeepch@gmail.com>
2025-07-02 22:26:30 +0530

    Learn about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14234
+let accountEmail = "hello@madhurjya.com"
+var accountPassword = "password123"
+accountCity = "Jaipur"
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* 
+Prefer not to use var, as it has functional scope and block scope issues.
+*/
+
