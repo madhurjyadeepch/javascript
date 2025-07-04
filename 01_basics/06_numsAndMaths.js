d9fcc757118fdee86175aff525d0e3619c445efd
Madhurjya Deep Chakravarty <madhurjyadeepch@gmail.com>
2025-07-04 22:08:27 +0530

    Maths and Num

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,35 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(typeof (balance))
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(2))
+
+otherNumber = 1240.2123
+
+console.log(otherNumber.toPrecision(5));
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString());
+
+
+// +++++++++++++++++++++++++++ Maths ++++++++++++++++++++++ //
+
+console.log(Math)
+
+console.log(Math.random())
+
+const max = 6
+const min = 1
+
+console.log(Math.floor((Math.random()* (max - min + 1)) + min))
+
+
+
+
+
