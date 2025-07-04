93d8eb2f64109d2f67354c574e4967cd9640be76
Madhurjya Deep Chakravarty <madhurjyadeepch@gmail.com>
2025-07-04 17:08:57 +0530

    Data Types and Memory Types

---
+++ 05_memorytypes.js
@@ -0,0 +1,3 @@
+// Stack memory (Used by primitive data types) => Gives a copy and then change the value of the copy.
+
+// Heap memory (Used by non-primitive data types) => Directly changes the value of the reference.
